1cbcdd22cd9b622c4c88be463e1f00d3504d5eec
Sam Russell <sam.h.russell@gmail.com>
2013-02-18 21:41:24 +1300

    Notes & text parsing
    Making notes in a separate file
    Parsing Tj and TJ objects, trying to figure out how this all works

--- samshares.py
+++ samshares.py
@@ -5,6 +5,7 @@
 # I plan to update PyPDF2 to suit my needs here
 
 import re
+import pprint
 
 import sys
 import os.path
@@ -18,27 +19,38 @@ import PyPDF2
 # Apparently pdf.getPage(i) gets me a PageObject
 # we can use this to extract more stuff somehow...
 #
-# PageObject subclasses DictionaryObject
-# DictionaryObject.arr is a dictionary with stuff in it
-# tempted to just JSONprint it...
-#
-# Let's look through PageObject.extractText() and get something
-# useful out of it
-#
-# Wrapped it, text objects are char at a time, looking a bit deeper:
-# pdf.py has _decryptobject(), see if this stores co-ordinates
+
 
 f = open('pdfdetails.txt', 'r')
 line = f.readline()
 (filename, num) = line.split(':')
 pdf = PyPDF2.PdfFileReader(file(filename, 'rb'))
 page = pdf.getPage(int(num))
-# get array of TextStringObject
-tso = page.getTextObjects()
-# print and see what happens
 
-for bit in tso:
-  print ("Object: %(bit)s" % locals()).encode('ascii',errors='backslashreplace')
+# Let's write functions to parse Tj and TJ
+
+def parsePDFLine(operator, operands):
+  if operator=='Tj':
+    bigstring = ''.join(operands)
+    return "String \"%s\" <%s>" % (unicode(bigstring).encode('ascii',errors='backslashreplace'), operator)
+  elif operator=='TJ':
+    # pattern is char spacing char spacing
+    # ignore spacing
+    #pprint.pprint(operands)
+    bigstring = ''.join(operands[0][::2])
+    #bigstring = ''.join(operands[::2])
+    return "String \"%s\" <%s>" % (unicode(bigstring).encode('ascii',errors='backslashreplace'), operator)
+  return "%s <%s>" % (unicode(operands).encode('ascii',errors='backslashreplace'), operator)
+
+content = page["/Contents"].getObject()
+if not isinstance(content, PyPDF2.pdf.ContentStream):
+    content = PyPDF2.pdf.ContentStream(content, page.pdf)
+
+for operands,operator in content.operations:
+  # let's find out how the Tj TJ T* operators work...
+  print parsePDFLine(operator, operands)
+  #print unicode(operands).encode('ascii',errors='backslashreplace'),
+  #print ("<%s>" % operator)
 
 #print page.extractText().encode('ascii',errors='backslashreplace')
 
