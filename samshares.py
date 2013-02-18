4330c9519a9f766b7f128f45c382b34849e00652
Sam Russell <sam.h.russell@gmail.com>
2013-02-18 22:38:08 +1300

    Got locations, next step is rendering

--- samshares.py
+++ samshares.py
@@ -42,6 +42,7 @@ def parsePDFLine(operator, operands, location):
     toreturn = "String (%d, %d)  \"%s\" <%s>" % (location[0], location[1], unicode(bigstring).encode('ascii',errors='backslashreplace'), operator)
   elif operator=='BT':
     # reset location
+    baselocation = [0,0]
     location = [0,0]
     toreturn = "Begin text"
   elif operator=='ET':
@@ -64,11 +65,45 @@ content = page["/Contents"].getObject()
 if not isinstance(content, PyPDF2.pdf.ContentStream):
     content = PyPDF2.pdf.ContentStream(content, page.pdf)
 
+baselocation = [0,0]
 location = [0,0]
 
 for operands,operator in content.operations:
   # let's find out how the Tj TJ T* operators work...
-  (toprint, location) = parsePDFLine(operator, operands, location)
+  if operator=='Tj':
+    bigstring = ''.join(operands)
+    toprint = "String (%d, %d) \"%s\" <%s>" % (location[0], location[1], unicode(bigstring).encode('ascii',errors='backslashreplace'), operator)
+  elif operator=='TJ':
+    # pattern is char spacing char spacing
+    # ignore spacing
+    #pprint.pprint(operands)
+    bigstring = ''.join(operands[0][::2])
+    #bigstring = ''.join(operands[::2])
+    toprint = "String (%d, %d) \"%s\" <%s>" % (location[0], location[1], unicode(bigstring).encode('ascii',errors='backslashreplace'), operator)
+  elif operator=='BT':
+    # reset location
+    baselocation = [0,0]
+    location = [0,0]
+    toprint = "Begin text"
+  elif operator=='ET':
+    toprint = "End text"
+  elif operator=='Tm':
+    # 6 fields in array
+    # x = a*x + c*y + e
+    # x = b*x + d*y + f
+    newlocation = []
+    newlocation.append(location[0]*operands[0] + location[1]*operands[2] + operands[4])
+    newlocation.append(location[0]*operands[1] + location[1]*operands[3] + operands[5])
+    baselocation = newlocation
+    location = newlocation
+    toprint = "New location: (%d, %d) <%s>" % (location[0], location[1], operator)
+  # need to still do Tc, Tw, Td
+  # Td = move to next line with stuff (5.3.1)
+  # Tc = charSpace, Tw = wordSpace (5.2.1)
+  elif operator=='Td':
+    pass
+  else:
+    toprint = "%s <%s>" % (unicode(operands).encode('ascii',errors='backslashreplace'), operator)
   print toprint
   #print unicode(operands).encode('ascii',errors='backslashreplace'),
   #print ("<%s>" % operator)
